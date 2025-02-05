99b3880a15ed6190f0c3c7e062de61ef84876eef
Daemons <143678373+ChronosWithA-K@users.noreply.github.com>
2025-02-04 21:03:58 -0800

    work on displayInfo()

--- Game.java
+++ Game.java
@@ -7,7 +7,16 @@ public class Game {
     private int secretLength;
     private final String[] validGuesses = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
                                            "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-    private int lives = 5;
+
+    private int lives = 6;
+    private String hangState1; // Empty noose
+    private String hangState2;
+    private String hangState3;
+    private String hangState4;
+    private String hangState5;
+    private String hangState6;
+    private String hangState7; // Fully hung
+
     boolean first_game = true;
 
     /**
@@ -38,14 +47,20 @@ public class Game {
      */
     public void makeGuess() {
         System.out.print("Enter your guess: ");
-        String guess = sc.nextLine();
-        guess = guess.toLowerCase();
+        String guess = sc.nextLine().toLowerCase();
         boolean guessValid = false;
 
         while (!guessValid) {
             if (validateGuess(guess)) {
                 guessValid = true;
             }
+        }
+
+        if (secret.contains(guess)) {
+            displayInfo();
+        } else {
+            System.out.println("Incorrect guess.");
+            lives--;
         }
     }
 
@@ -67,5 +82,28 @@ public class Game {
             }
         }
         return testsPassed == 2;
+    }
+
+    /**
+     * Displays how far along the hanging has progressed, and the progress in guessing the secret.
+     */
+    public void displayInfo() {
+        if (lives == 6) {
+            System.out.println(hangState1);
+        } else if (lives == 5) {
+            System.out.println(hangState2);
+        } else if (lives == 4) {
+            System.out.println(hangState3);
+        } else if (lives == 3) {
+            System.out.println(hangState4);
+        } else if (lives == 2) {
+            System.out.println(hangState5);
+        } else if (lives == 1) {
+            System.out.println(hangState6);
+        } else {
+            System.out.println(hangState7);
+        }
+
+
     }
 }
--- PlayGame.java
+++ PlayGame.java
@@ -1,7 +1,12 @@
 public class PlayGame {
     public static void main(String[] args) {
         Game game = new Game();
+        boolean playerWantsToPlay = true;
 
-
+        while (playerWantsToPlay) {
+            game.setWord();
+            game.displayStartInfo();
+            game.makeGuess();
+        }
     }
 }
