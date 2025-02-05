e1120e8db30d45b07d7274daec1eccf14bc4b358
Daemons <143678373+ChronosWithA-K@users.noreply.github.com>
2025-02-04 19:02:34 -0800

    finish validateGuess() method

--- Game.java
+++ Game.java
@@ -9,10 +9,10 @@ public class Game {
 
     private String secret;
     private int secretLength;
-//    private ArrayList<String> validGuesses = new ArrayList<String>();
+    //    private ArrayList<String> validGuesses = new ArrayList<String>();
 //    validGuesses.add()
     private final String[] validGuesses = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-        "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+            "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 
     boolean first_game = true;
 
@@ -30,9 +30,9 @@ public class Game {
     public void displayStartInfo() {
         if (first_game) {
             System.out.println("This is a standard game of Hangman. You guess what letters are in a secret word, and I'll " +
-                               "tell you whether or not the letter appears in the word. Your guess has to be a single letter. " +
-                               "If your guess appears in the word, I'll show you where and how many times. If it doesn't, " +
-                               "you lose one of your guesses. Guess all the letters in the word to win.");
+                    "tell you whether or not the letter appears in the word. Your guess has to be a single letter. " +
+                    "If your guess appears in the word, I'll show you where and how many times. If it doesn't, " +
+                    "you lose one of your guesses. Guess all the letters in the word to win.");
             first_game = false;
         } else {
             System.out.println("I hope you have a fun time playing!");
@@ -43,23 +43,30 @@ public class Game {
         System.out.print("Enter your guess: ");
         String guess = sc.nextLine();
         guess = guess.toLowerCase();
-                (guess.length() != 1) || (Arrays.stream(validGuesses).noneMatch(guess::equals);
-                break;
-            } catch (Exception e) {
-                System.out.println("Invalid guess, please try again. Remember, your guess has to be a single letter.");
-            }
+        boolean guessValid = false;
 
-        while (true) {
-            if ((guess.length() != 1) || (Arrays.stream(validGuesses).noneMatch(guess::equals))) {
-                System.out.println("Invalid guess. Try again.");
-                guess = sc.nextLine();
-            } else {
-                break;
+        while (!guessValid) {
+            if (validateGuess(guess)) {
+                guessValid = true;
             }
         }
     }
 
     private boolean validateGuess(String guess) {
+        int testsPassed = 0;
+        if (guess.length() == secretLength) {
+            testsPassed++;
+        }
 
+        for (int i = 0; i < validGuesses.length; i++) {
+            if (guess.equals(validGuesses[i])) {
+                testsPassed++;
+                break;
+            }
+        }
+        if (testsPassed == 2) {
+            return true;
+        }
+        return false;
     }
 }
